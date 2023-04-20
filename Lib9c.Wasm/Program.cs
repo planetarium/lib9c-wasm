c39f6b59f12f85970e35dcb3e5ea828af74edb9c
Akamig <akamig@posteo.net>
2023-04-20 18:54:26 +0900

    Fixed to comply with native wasm interop method + format

--- Lib9c.Wasm/Program.cs
+++ Lib9c.Wasm/Program.cs
@@ -1,15 +1,18 @@
 using System.Reflection;
 using System.Text.Json;
+using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.Versioning;
 using Bencodex;
-using DotNetJS;
 using Libplanet.Action;
-using Microsoft.JSInterop;
 using Nekoyume.Action;
 using static Lib9c.Wasm.JsonUtils;
 
 namespace Lib9c.Wasm;
-public class Program
+
+[SupportedOSPlatform("browser")]
+public partial class Program
 {
+
     public static void Main()
     {
         JS.Runtime.ConfigureJson(options =>
@@ -20,7 +23,7 @@ public class Program
 
     public record Input(string Name, string Type);
 
-    [JSInvokable]
+    [JSExport]
     public static string[] GetAllActionTypes()
     {
         var types = typeof(Nekoyume.Action.ActionBase).Assembly.GetTypes()
@@ -28,7 +31,7 @@ public class Program
         return types.Select(x => ActionTypeAttribute.ValueOf(x)).Where(x => x is Bencodex.Types.Text).Select(x => ((Bencodex.Types.Text)x).Value).ToArray();
     }
 
-    [JSInvokable]
+    [JSExport]
     public static string GetAvailableInputs(string actionTypeString)
     {
         Type actionType = typeof(Nekoyume.Action.ActionBase).Assembly.GetTypes()
@@ -37,13 +40,13 @@ public class Program
         return ResolveType(actionType);
     }
 
-    [JSInvokable]
-    public static byte[] BuildAction(string actionTypeString, JsonElement dictionary)
+    [JSExport]
+    public static byte[] BuildAction(string actionTypeString, [JSMarshalAsAttribute<JSType.Any>] Object dictionary)
     {
         Type actionType = typeof(Nekoyume.Action.ActionBase).Assembly.GetTypes()
             .First(t => t.IsDefined(typeof(ActionTypeAttribute)) && ActionTypeAttribute.ValueOf(t) is Bencodex.Types.Text text && text.Value == actionTypeString);
 
-        var action = (IAction)ConvertJsonElementTo(dictionary, actionType);
+        var action = (IAction)dictionary;
         return new Codec().Encode(((PolymorphicAction<ActionBase>)(dynamic)action).PlainValue);
     }
 }
