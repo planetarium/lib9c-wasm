9ffef356f241824877497f39fddcb8cd68be8a84
moreal <dev.moreal@gmail.com>
2023-05-24 14:07:03 +0900

    Remove unused code

--- Lib9c.Wasm/Program.cs
+++ Lib9c.Wasm/Program.cs
@@ -31,33 +31,8 @@ public class Program
     [JSInvokable]
     public static string GetAvailableInputs(string actionTypeString)
     {
-        bool IsIgnoredVariableName(string name)
-        {
-            return name == "errors"
-                || name == "PlainValue"
-                || name == "PlainValueInternal"
-                || name.StartsWith("Extra");
-        }
-
-        bool IsIgnoredType(Type type)
-        {
-            return type.Name.EndsWith("BattleLog")
-                || type.Name.EndsWith("Result")
-                || type.Name.EndsWith("AvatarState")
-                || type.Name.EndsWith("ArenaInfo")
-                || type.Name.EndsWith("Digest");
-        }
-
         Type actionType = typeof(Nekoyume.Action.ActionBase).Assembly.GetTypes()
             .First(t => t.IsDefined(typeof(ActionTypeAttribute)) && ActionTypeAttribute.ValueOf(t) is Bencodex.Types.Text text && text.Value == actionTypeString);
-
-        var fields = actionType.GetFields(BindingFlags.Public | BindingFlags.Instance).Where(f => f.IsPublic && !IsIgnoredVariableName(f.Name) && !IsIgnoredType(f.FieldType));
-        var properties = actionType.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).Where(p => p.CanWrite && !IsIgnoredVariableName(p.Name) && !IsIgnoredType(p.PropertyType));
-        // Map fields and properties to their TypeScript types
-        var inputs = fields.Select(f => $"{f.Name}: {ResolveType(f.FieldType)}").Concat(properties.Select(p => $"{p.Name}: {ResolveType(p.PropertyType)}")).ToArray();
-
-        // Combine the TypeScript types into a single object type
-        var typeInfo = "{" + string.Join(", ", inputs) + "}";
 
         return ResolveType(actionType);
     }
