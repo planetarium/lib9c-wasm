d82bdb94165a176e9449906e66355d6ae9c7d920
Akamig <akamig@posteo.net>
2023-08-25 13:43:37 +0900

    Bump lib9c, libplanet

--- Lib9c.Wasm/Program.cs
+++ Lib9c.Wasm/Program.cs
@@ -2,9 +2,24 @@ using System.Reflection;
 using System.Text.Json;
 using Bencodex;
 using DotNetJS;
-using Libplanet.Action;
 using Microsoft.JSInterop;
+using Libplanet.Action;
+using Libplanet.Action.State;
+using Libplanet.Crypto;
+using Libplanet.Types.Assets;
+using Nekoyume;
 using Nekoyume.Action;
+using Nekoyume.Battle;
+using Nekoyume.Extensions;
+using Nekoyume.Action;
+using Nekoyume.Model;
+using Nekoyume.Model.Item;
+using Nekoyume.Model.Mail;
+using Nekoyume.Model.Quest;
+using Nekoyume.Model.Rune;
+using Nekoyume.Model.Skill;
+using Nekoyume.Model.State;
+using Nekoyume.TableData;
 using static Lib9c.Wasm.JsonUtils;
 
 namespace Lib9c.Wasm;
@@ -25,14 +40,14 @@ public class Program
     {
         var types = typeof(Nekoyume.Action.ActionBase).Assembly.GetTypes()
             .Where(t => t.IsDefined(typeof(ActionTypeAttribute)) && t != typeof(InitializeStates) && t != typeof(CreatePendingActivations));
-        return types.Select(x => ActionTypeAttribute.ValueOf(x)).Where(x => x is Bencodex.Types.Text).Select(x => ((Bencodex.Types.Text)x).Value).ToArray();
+        return types.Select(x => x.GetCustomAttribute<ActionTypeAttribute>()?.TypeIdentifier).Where(x => x is Bencodex.Types.Text).Select(x => ((Bencodex.Types.Text)x).Value).ToArray();
     }
 
     [JSInvokable]
     public static string GetAvailableInputs(string actionTypeString)
     {
         Type actionType = typeof(Nekoyume.Action.ActionBase).Assembly.GetTypes()
-            .First(t => t.IsDefined(typeof(ActionTypeAttribute)) && ActionTypeAttribute.ValueOf(t) is Bencodex.Types.Text text && text.Value == actionTypeString);
+            .First(t => t.IsDefined(typeof(ActionTypeAttribute)) && t.GetCustomAttribute<ActionTypeAttribute>()?.TypeIdentifier is Bencodex.Types.Text text && text.Value == actionTypeString);
 
         return ResolveType(actionType);
     }
@@ -41,9 +56,11 @@ public class Program
     public static byte[] BuildAction(string actionTypeString, JsonElement dictionary)
     {
         Type actionType = typeof(Nekoyume.Action.ActionBase).Assembly.GetTypes()
-            .First(t => t.IsDefined(typeof(ActionTypeAttribute)) && ActionTypeAttribute.ValueOf(t) is Bencodex.Types.Text text && text.Value == actionTypeString);
+            .First(t => t.IsDefined(typeof(ActionTypeAttribute)) && t.GetCustomAttribute<ActionTypeAttribute>()?.TypeIdentifier is Bencodex.Types.Text text && text.Value == actionTypeString);
 
         var action = (IAction)ConvertJsonElementTo(dictionary, actionType);
-        return new Codec().Encode(((PolymorphicAction<ActionBase>)(dynamic)action).PlainValue);
+        return new Codec().Encode(((ActionBase)(dynamic)action).PlainValue);
     }
+
+  
 }
