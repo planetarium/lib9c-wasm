cc6fc4f29e642a0b023428e8dc8b982f06da736b
Akamig <akamig@posteo.net>
2023-09-03 17:57:46 +0900

    Executable useStageSimulatorV3 (need table loader)

--- Lib9c.Wasm/Program.cs
+++ Lib9c.Wasm/Program.cs
@@ -1,8 +1,10 @@
 using System.Reflection;
+using System.Security.Cryptography;
 using System.Text.Json;
 using Bencodex;
 using DotNetJS;
 using Microsoft.JSInterop;
+using Libplanet.Common;
 using Libplanet.Action;
 using Libplanet.Action.State;
 using Libplanet.Crypto;
@@ -20,6 +22,8 @@ using Nekoyume.Model.Rune;
 using Nekoyume.Model.Skill;
 using Nekoyume.Model.State;
 using Nekoyume.TableData;
+using Lib9c.Tests;
+using Lib9c.Tests.Action;
 using static Lib9c.Wasm.JsonUtils;
 
 namespace Lib9c.Wasm;
@@ -62,5 +66,49 @@ public class Program
         return new Codec().Encode(((ActionBase)(dynamic)action).PlainValue);
     }
 
-  
+    [JSInvokable]
+    public static string UseStageSimulatorV3(string avatarState, string PreEvaluationHash, string signature, int worldId, int stageId)
+    {
+        var preEvaluationHashBytes = ByteUtil.ParseHex(PreEvaluationHash);
+        byte[] hashedSignature;
+        using (var hasher = SHA1.Create())
+        {
+            hashedSignature = hasher.ComputeHash(ByteUtil.ParseHex(signature));
+        }
+
+        var _sheets = TableSheetsImporter.ImportSheets();
+        var _tableSheets = new TableSheets(_sheets);
+        var state = (Bencodex.Types.Dictionary)new Codec().Decode(ByteUtil.ParseHex(avatarState));
+        var _avatarState = new AvatarState(state);
+
+        var skillsOnWaveStart = new List<Skill>();
+
+        var seed = (preEvaluationHashBytes.Length > 0
+                    ? BitConverter.ToInt32(preEvaluationHashBytes, 0) : 0)
+                ^ (signature.Any() ? BitConverter.ToInt32(hashedSignature, 0) : 0) - 0;
+        var contextRandom = new TestRandom(seed);
+
+        var simulator = new StageSimulatorV3(
+                contextRandom,
+                _avatarState,
+                new List<Guid>(),
+                null,
+                skillsOnWaveStart,
+                worldId,
+                stageId,
+                _tableSheets.StageSheet[stageId],
+                _tableSheets.StageWaveSheet[stageId],
+                false,
+                StageRewardExpHelper.GetExp(_avatarState.level, stageId),
+                _tableSheets.GetSimulatorSheets(),
+                _tableSheets.EnemySkillSheet,
+                _tableSheets.CostumeStatSheet,
+                StageSimulatorV3.GetWaveRewards(
+                    contextRandom,
+                    _tableSheets.StageSheet[stageId],
+                    _tableSheets.MaterialItemSheet));
+        simulator.Simulate();
+        var log = simulator.Log;
+        return string.Format("", log.result, log.clearedWaveNumber);
+    }
 }
